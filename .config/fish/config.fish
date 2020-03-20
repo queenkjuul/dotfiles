154c5a9f64b80461b441ff1759989f3fc9f4b000
queenkjuul <julie.hill@pm.me>
2020-03-20 11:52:42 -0500

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
 abbr mysql /usr/local/mysql/bin/mysql
 abbr git hub
-abbr dotfiles 'git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+abbr dot 'git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 back 'cd -'
